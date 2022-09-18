564fcef9df92f8d70c31d2c27bf64a0f36b26f9f
Jay <tddwm1990@gmail.com>
2022-09-19 02:05:47 +0800

    FAMS5-8930: Jay - push up laravel base library

---
+++ jenssegers/mongodb/src/Jenssegers/Mongodb/Auth/DatabaseTokenRepository.php
@@ -0,0 +1,37 @@
+<?php namespace Jenssegers\Mongodb\Auth;
+
+use MongoDB\BSON\UTCDateTime;
+
+class DatabaseTokenRepository extends \Illuminate\Auth\Passwords\DatabaseTokenRepository
+{
+    /**
+     * Build the record payload for the table.
+     *
+     * @param  string  $email
+     * @param  string  $token
+     * @return array
+     */
+    protected function getPayload($email, $token)
+    {
+        return ['email' => $email, 'token' => $token, 'created_at' => new UTCDateTime(round(microtime(true) * 1000))];
+    }
+
+    /**
+     * Determine if the token has expired.
+     *
+     * @param  array  $token
+     * @return bool
+     */
+    protected function tokenExpired($token)
+    {
+        // Convert UTCDateTime to a date string.
+        if ($token['created_at'] instanceof UTCDateTime) {
+            $date = $token['created_at']->toDateTime();
+            $token['created_at'] = $date->format('Y-m-d H:i:s');
+        } elseif (is_array($token['created_at']) and isset($token['created_at']['date'])) {
+            $token['created_at'] = $token['created_at']['date'];
+        }
+
+        return parent::tokenExpired($token);
+    }
+}
